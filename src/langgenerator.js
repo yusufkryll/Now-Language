a304eab4dd1fa362acbc7a3245e34c3b002778dd
JSDevloop <56578188+JSDevloop@users.noreply.github.com>
2020-05-02 14:42:30 +0300

    somethings

--- src/langgenerator.js
+++ src/langgenerator.js
@@ -1,156 +1,157 @@
 const fs = require('fs');
-const readline = require("readline");
+const readline = require('readline');
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout,
 });
-let lang = function(){
-	this.compilelang = "js";
-	this.ins = {
-		end:";",
-		string:"'",
-	};
-	this.strs = {
-		if:"if",
-		function:"fn",
-		end:"$"
-	};
-	this.start = (run) =>{
-		var code = "";
-		if(process.argv[2]!=null){
-			rl.close();
-			fs.readFile(process.argv[2], (err, data) => {
-				if (err) {
-					console.error(err);
-					return;
-				}
-				this.compilelang = "js";
-				code = data.toString();
-				var out = run(code);
-				if(process.argv[3]=="--getjsandrun") {
-					fs.writeFile(process.argv[2].replace(".now","") + '.' + this.compilelang, out, function (err) {
-						if (err) throw err;
-						eval(out);
-					  }); 	
-				}
-				else if(process.argv[3]=="--getjs") {
-					fs.writeFile(process.argv[2].replace(".now","") + '.' + this.compilelang, out, function (err) {
-						if (err) throw err;
-					  }); 	
-				}
-				else{
-					eval(out);
-				}
-			});
-		}else{
-			function reed() {
-				rl.question("> ", function(input) {
-					var out = run(input);
-					try {
-						eval(out); 
-					} catch (e) {
-							console.log(e.message);
-					}
-					reed();
-				});	
-			}
-			reed();
-		}
-	};
-	var infun = false;
-	var instr = false;
-	//kodu parçalara ayırma
-	this.part = (code) => {
-		var result = [];
-		var csplit = code.split("");
-		var opn = 0;
-		var cls = 0;
-		for(elm in csplit){
-			if(csplit[elm] == ";" && !infun && !instr){
-				var until = csplit.slice(0,parseInt(elm));
-				var devam = csplit.slice(parseInt(elm)+1);
-				result.push(until.join(""));
-				result.push(this.part(devam.join(""),()=>{}));
-				var merged = result.flat(Infinity);
-				return merged;
-			}
-			if(csplit[elm] == "{" && !instr){
-				infun = true;
-				opn++;
-			}
-			if(csplit[elm] == "\""){
-				instr = !instr;
-			}
-			if(csplit[elm] == "}" && !instr){
-				cls++;
-				if(opn==cls){
-					var until = csplit.slice(0,parseInt(elm));
-					var devam = csplit.slice(parseInt(elm)+1);
-					result.push(until.join("").replace(/{/,""));
-					infun = false;
-					result.push(this.part(devam.join("")));
-					var merged = result.flat(Infinity);
-					return merged;
-				}
-			}
-		}
-		return "";
-	}
-	this.analysis = (code,fn) => {
-		let codesplitbyspaces = code.replace(/\#\*[\s\S]*?\*\#|\#.*/g, '');
-		codesplitbyspaces = codesplitbyspaces.replace(/(\r\n|\n|\r)/gm, "").splitEx(" {}");
-		let conlysp = code.replace(/(\r\n|\n|\r)/gm, "").splitEx(" ");
-		for(x in codesplitbyspaces){
-			// if(codesplitbyspaces[x] != " ") codesplitbyspaces[x]=codesplitbyspaces[x].trim();
-		}
-		for(ch in codesplitbyspaces){
-			var a = {
-				word:codesplitbyspaces[ch],
-				number:parseInt(ch),
-				words:codesplitbyspaces,
-				words1:conlysp,
-				getafterto:function(arg1,arg2) {
-					var after = this.words.slice(arg1);
-					var result = "";
-					for(elm in after){
-						if(elm == after.lastIndexOf(arg2)) return result;
-						result+=after[elm];
-					}
-				},
-				getaftertof:function(arg1,arg2) {
-					var after = this.words.slice(arg1);
-					var result = "";
-					for(elm in after){
-						if(elm == after.indexOf(arg2)) return result;
-						result+=after[elm];
-					}
-				},
-				getafter: function(arg1) {
-					var after = this.words.slice(arg1);
-					var result = "";
-					for(elm in after){
-						result+=after[elm];
-					}
-					return result;
-				}
-			};
-			fn(a);
-		}
-	}
+const Lang = function() {
+  this.compilelang = 'js';
+  this.ins = {
+    end: ';',
+    string: '\'',
+  };
+  this.strs = {
+    if: 'if',
+    function: 'fn',
+    end: '$',
+  };
+  this.start = (run) =>{
+    let code = '';
+    if (process.argv[2]!=null) {
+      rl.close();
+      fs.readFile(process.argv[2], (err, data) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        this.compilelang = 'js';
+        code = data.toString();
+        const out = run(code);
+        if (process.argv[3]=='--getjsandrun') {
+          fs.writeFile(process.argv[2]
+			  .replace('.now', '') + '.' + this.compilelang, out,
+          function(err) {
+            if (err) throw err;
+            eval(out);
+					  });
+        } else if (process.argv[3]=='--getjs') {
+          fs.writeFile(process.argv[2].replace('.now', '') + '.' +
+			   this.compilelang, out,
+			   function(err) {
+            if (err) throw err;
+					  });
+        } else {
+          eval(out);
+        }
+      });
+    } else {
+      function reed() {
+        rl.question('> ', function(input) {
+          const out = run(input);
+          try {
+            eval(out);
+          } catch (e) {
+            console.log(e.message);
+          }
+          reed();
+        });
+      }
+      reed();
+    }
+  };
+  let infun = false;
+  let instr = false;
+  // kodu parçalara ayırma
+  this.part = (code) => {
+    const result = [];
+    const csplit = code.split('');
+    let opn = 0;
+    let cls = 0;
+    for (elm in csplit) {
+      if (csplit[elm] == ';' && !infun && !instr) {
+        var until = csplit.slice(0, parseInt(elm));
+        var devam = csplit.slice(parseInt(elm)+1);
+        result.push(until.join(''));
+        result.push(this.part(devam.join(''), ()=>{}));
+        var merged = result.flat(Infinity);
+        return merged;
+      }
+      if (csplit[elm] == '{' && !instr) {
+        infun = true;
+        opn++;
+      }
+      if (csplit[elm] == '"') {
+        instr = !instr;
+      }
+      if (csplit[elm] == '}' && !instr) {
+        cls++;
+        if (opn==cls) {
+          var until = csplit.slice(0, parseInt(elm));
+          var devam = csplit.slice(parseInt(elm)+1);
+          result.push(until.join('').replace(/{/, ''));
+          infun = false;
+          result.push(this.part(devam.join('')));
+          var merged = result.flat(Infinity);
+          return merged;
+        }
+      }
+    }
+    return '';
+  };
+  this.analysis = (code, fn) => {
+    let codesplitbyspaces = code.replace(/\#\*[\s\S]*?\*\#|\#.*/g, '');
+    codesplitbyspaces = codesplitbyspaces
+        .replace(/(\r\n|\n|\r)/gm, '')
+        .splitEx(' {}');
+    const conlysp = code.replace(/(\r\n|\n|\r)/gm, '').splitEx(' ');
+    for (ch in codesplitbyspaces) {
+      const a = {
+        word: codesplitbyspaces[ch],
+        number: parseInt(ch),
+        words: codesplitbyspaces,
+        words1: conlysp,
+        getafterto: function(arg1, arg2) {
+          const after = this.words.slice(arg1);
+          let result = '';
+          for (elm in after) {
+            if (elm == after.lastIndexOf(arg2)) return result;
+            result+=after[elm];
+          }
+        },
+        getaftertof: function(arg1, arg2) {
+          const after = this.words.slice(arg1);
+          let result = '';
+          for (elm in after) {
+            if (elm == after.indexOf(arg2)) return result;
+            result+=after[elm];
+          }
+        },
+        getafter: function(arg1) {
+          const after = this.words.slice(arg1);
+          let result = '';
+          for (elm in after) {
+            result+=after[elm];
+          }
+          return result;
+        },
+      };
+      fn(a);
+    }
+  };
 };
 
 String.prototype.splitEx = function(delimiters) {
-    var parts = [];
-    var current = '';
-    for (var i = 0; i < this.length; i++) {
-        if (delimiters.indexOf(this[i]) < 0) current += this[i];
-        else {
-            parts.push(current);
-            current = this[i];
-        }
+  const parts = [];
+  let current = '';
+  for (let i = 0; i < this.length; i++) {
+    if (delimiters.indexOf(this[i]) < 0) current += this[i];
+    else {
+      parts.push(current);
+      current = this[i];
     }
-    parts.push(current);
-    return parts;
+  }
+  parts.push(current);
+  return parts;
 };
 
-exports.lang = lang;
+exports.lang = Lang;
--- src/nowlang.js
+++ src/nowlang.js
@@ -1,144 +1,256 @@
 #!/usr/bin/env node
-/*jshint esversion: 6 */
+/* jshint esversion: 6 */
 
-var lang = require("./langgenerator.js").lang;
+const Lang = require('./langgenerator.js').Lang;
 const fs = require('fs');
-var nowlang = new lang();
-var out = "";
-nowlang.start((code)=>{
-    out = partcode(code);
-    return out;
+const Nowlang = new Lang();
+let out = '';
+Nowlang.start((code)=>{
+  out = partcode(code);
+  return out;
 });
-
-function partcode(code){
-    var results = "";
-    var parted = nowlang.part(code);
-    Array.from(parted).forEach(element => {
-        results+=compile(element); 
-    });
-    return results;
+/**
+ * @param {string} code - NowLang code.
+ * @return {string} Parted NowLang code.
+ */
+function partcode(code) {
+  let results = '';
+  const parted = Nowlang.part(code);
+  Array.from(parted).forEach((element) => {
+    results+=compile(element);
+  });
+  return results;
 }
+/**
+ * @param {string} code - Parted NowLang code.
+ * @return {string} NowLang to javascript code.
+ */
 function compile(code) {
-    var result = "";
-    var first = true;
-    nowlang.analysis(code,pack => {
-        if(first){
-            first = false;
-            var _strt = "";
-            var _end = "";
-            var looptime = pack.words.join("").trim().split(/([)])/).pop().split("::")[1];
-            if(pack.words.join("").split("(")[0].trim() != "if" && pack.words.join("").split("(")[0].trim() != "layer" && pack.words.join("").split("(")[0].trim() != "loop" && !pack.words.join("").startsWith("else") && pack.words.join("").split("(")[0].trim() != "else if" && pack.words.join("").split("(")[0].trim() != "while" && pack.words.join("").split("(")[0].trim() != "for"){
-                if(looptime != undefined){
-                    pack.words[pack.words.length-1] = pack.words.join("").trim().split(":").slice(0,1).join("");
-                    if(looptime.substring(0,1)==">"){
-                        looptime = looptime.substring(1);
-                        _strt = `for(freeloop=0;freeloop<${looptime};freeloop++){`;
-                    }else{
-                        _strt = `for(freeloop=${looptime};freeloop>0;freeloop--){`;
-                    }
-                    _end = `}`;
-                }
-            }
-            if(pack.words.join("").trim().split(" ")[0] == "var"){
-                let all = pack.words.join("");
-                all = all.replace("<<" , "{");
-                all = all.replace(">>" , "}");
-                result += _strt + all + ";\n";
-                
-            }
-            else if(pack.words[0] == "#"){
-            }
-            else if(pack.words[0] == "return"){
-                result += "return" + pack.words.slice(1).join("") + ";\n";
-            }
-            else if(pack.words.join("").split("(")[0].trim() == "delay"){
-                let body = pack.words.join("").split("(").slice(1); 
-                body[0] = "(" + body[0];
-                result += _strt + "await new Promise(resolve => setTimeout(resolve, " + body.join("") + "));\n" + _end;
-            }
-            else if(pack.words.join("").split("(")[0].trim() == "onJS"){
-                let body = pack.words.join("").split(/([(])/).slice(1);
-                body[0] = body[0];
-                result += _strt + body.join("").substring(2, body.join("").length-2); + "\n" + _end; // jshint ignore:line
-            }
-            else if(pack.words[0] == "add"){
-                result += partcode(fs.readFileSync(pack.words.slice(1).join("").split("\"").join("").trim()).toString());
-            }
-            else if(pack.words[0] == "fn"){
-                var name = pack.words.slice(1).join("").split("(")[0].trim();
-                let args = pack.words.join("").split(/([(])/).slice(1).join("").split(/([)])/);
-                args = args[0] + args[1];
-                if(args == undefined) args = "";
-                if(name == undefined) name = "";
-                result += "function " + name + args + "\n" +
-                 "{\n" + partcode(pack.words.join("").split(/([)])/).slice(2).join("")) + "}\n";
-            }
-            else if(pack.words.join("").split("(")[0].trim() == "layer"){
-                
-                let args = pack.words.join("").split(/([(])/).slice(1).join("").split(/([)])/);
-                args = args[0] + args[1];
-                if(args == undefined) args = "";
-                result += "(" + args.replace(/([)(])/g,"") + "()=>{\n" + partcode(pack.words.join("").split(/([)])/).slice(2).join("")) + "\n})();\n";
-            }
-            else if(pack.words.join("").split("(")[0].trim() == "loop"){
-                result += "setInterval(() => \n" + "{\n" + partcode(pack.words.join("").split(/([)])/).slice(2).join("")) + "},1);\n";
-            }
-            else if(pack.words.join("").split("(")[0].trim() == "if"){
-                let args = pack.words.join("").split(/([(])/).slice(1).join("").split(/([)])/);
-                args = args[0] + args[1];
-                if(args == undefined) args = "";
-                result += "if" + args + "\n" +
-                "{\n" + partcode(pack.words.join("").split(/([)])/).slice(2).join("")) + "}\n";
-            }
-            else if(pack.words.join("").split("(")[0].trim() == "else if"){
-                let args = pack.words.join("").split(/([(])/).slice(1).join("").split(/([)])/);
-                args = args[0] + args[1];
-                if(args == undefined) args = "";
-                result += "else if" + args + "\n" +
-                "{\n" + partcode(pack.words.join("").split(/([)])/).slice(2).join("")) + "}\n";
-            }
-            else if(pack.words.join("").startsWith("else")){
-                    result += "else" + "\n" +
-                    "{\n" + partcode(pack.words.join("").trim().substring(4)) + "}\n";
-            }
-            else if(pack.words.join("").split("(")[0].trim() == "while"){
-                let args = pack.words.join("").split(/([(])/).slice(1).join("").split(/([)])/);
-                args = args[0] + args[1];
-                if(args == undefined) args = "";
-                result += "while" + args + "\n" +
-                "{\n" + partcode(pack.words.join("").split(/([)])/).slice(2).join("")) + "}\n";
-            }
-            else if(pack.words.join("").split("(")[0].trim() == "for"){
-                let args = pack.words.join("").split(/([(])/).slice(1).join("").split(/([)])/);
-                if(!args[0].includes("in")) args[0]+=";i>=0;i--";
-                args = args[0] + args[1];
-                if(args == undefined) args = "";
-                result += "for" + args + "\n" +
-                "{\n" + partcode(pack.words.join("").split(/([)])/).slice(2).join("")) + "}\n";
-            }
-            else if(pack.words[0].trim() == "$"){
-                let args = pack.words[1];
-                if(args == undefined) args = "";
-                result += "if(" + pack.words.slice(1).join("").split(/([)])/)[0] + pack.words.join("").split(/([)])/)[1] + ")\n" +
-                 "{\n" + partcode(pack.words.join("").split(/([)])/).slice(2).join("")) + "}\n";
-            }
-            else{
-                var ops = ["=", "!", ">", "<","-","+"];
-                let all = pack.words.join("");
-                all = all.replace("<<" , "{");
-                all = all.replace(">>" , "}");
-                var sp = pack.words.join("").split(/([(])/);
-                let body = sp.slice(1);
-                if(ops.some(el => pack.words.join("").includes(el))) {
-                    all = all.replace(".=","="+pack.words[0].split(".")[0]+".");
-                    result += _strt + all  + _end + ";\n";
-                }
-                else if(pack.words.join("") != "") 
-                {
-                    result += _strt + sp[0] + body.join("") + ";\n" + _end;
-                }
-            }
+  let result = '';
+  let first = true;
+  Nowlang.analysis(code, (pack) => {
+    if (first) {
+      first = false;
+      let _strt = '';
+      let _end = '';
+      let looptime = pack.words.join('')
+          .trim()
+          .split(/([)])/)
+          .pop()
+          .split('::')[1];
+      if (pack.words.join('').split('(')[0].trim() != 'if' &&
+        pack.words.join('').split('(')[0].trim() != 'layer' &&
+        pack.words.join('').split('(')[0].trim() != 'loop' &&
+        !pack.words.join('').startsWith('else') &&
+        pack.words.join('').split('(')[0].trim() != 'else if' &&
+        pack.words.join('').split('(')[0].trim() != 'while' &&
+        pack.words.join('').split('(')[0].trim() != 'for') {
+        if (looptime != undefined) {
+          pack.words[pack.words.length-1] = pack.words.join('')
+              .trim()
+              .split(':')
+              .slice(0, 1)
+              .join('');
+          if (looptime.substring(0, 1)=='>') {
+            looptime = looptime.substring(1);
+            _strt = `for(freeloop=0;freeloop<${looptime};freeloop++){`;
+          } else {
+            _strt = `for(freeloop=${looptime};freeloop>0;freeloop--){`;
+          }
+          _end = `}`;
         }
-    });
-    return result;
+      }
+      if (pack.words.join('').trim().split(' ')[0] == 'var') {
+        let all = pack.words.join('');
+        all = all.replace('<<', '{');
+        all = all.replace('>>', '}');
+        result += _strt + all + ';\n';
+      } else if (pack.words[0] == '#') {
+      } else if (pack.words[0] == 'return') {
+        result += 'return' + pack.words.slice(1).join('') + ';\n';
+      } else if (pack.words.join('').split('(')[0].trim() == 'delay') {
+        const body = pack.words.join('').split('(').slice(1);
+        body[0] = '(' + body[0];
+        result += _strt +
+        'await new Promise(resolve => setTimeout(resolve, ' +
+        body.join('') + '));\n' + _end;
+      } else if (pack.words.join('').split('(')[0].trim() == 'onJS') {
+        const body = pack.words.join('').split(/([(])/).slice(1);
+        body[0] = body[0];
+        result += _strt +
+        body.join('').substring(2, body.join('').length-2); +
+        '\n' + _end; // jshint ignore:line
+      } else if (pack.words[0] == 'add') {
+        result += partcode(
+            fs.readFileSync(
+                pack.words.slice(1)
+                    .join('')
+                    .split('"')
+                    .join('')
+                    .trim(),
+            ).toString());
+      } else if (pack.words[0] == 'fn') {
+        let name = pack.words.slice(1).join('').split('(')[0].trim();
+        let args = pack.words.join('')
+            .split(/([(])/)
+            .slice(1)
+            .join('')
+            .split(/([)])/);
+        args = args[0] + args[1];
+        if (args == undefined) args = '';
+        if (name == undefined) name = '';
+        result += 'function ' + name + args + '\n' +
+                 '{\n' +
+                 partcode(
+                     pack.words
+                         .join('')
+                         .split(/([)])/)
+                         .slice(2)
+                         .join('')) +
+                  '}\n';
+      } else if (pack.words.join('').split('(')[0].trim() == 'layer') {
+        let args = pack.words
+            .join('')
+            .split(/([(])/)
+            .slice(1)
+            .join('')
+            .split(/([)])/);
+        args = args[0] + args[1];
+        if (args == undefined) args = '';
+        result += '(' + args.replace(/([)(])/g, '') +
+        '()=>{\n' +
+        partcode(
+            pack.words
+                .join('')
+                .split(/([)])/)
+                .slice(2)
+                .join(''),
+        ) + '\n})();\n';
+      } else if (pack.words.join('').split('(')[0].trim() == 'loop') {
+        result += 'setInterval(() => \n' +
+         '{\n' +
+         partcode(
+             pack.words
+                 .join('')
+                 .split(/([)])/)
+                 .slice(2)
+                 .join('')) +
+           '},1);\n';
+      } else if (pack.words.join('').split('(')[0].trim() == 'if') {
+        let args = pack.words
+            .join('')
+            .split(/([(])/)
+            .slice(1)
+            .join('')
+            .split(/([)])/);
+        args = args[0] + args[1];
+        if (args == undefined) args = '';
+        result += 'if' + args + '\n' +
+                '{\n' + partcode(
+            pack.words
+                .join('')
+                .split(/([)])/)
+                .slice(2)
+                .join(''),
+        ) + '}\n';
+      } else if (pack.words.join('').split('(')[0].trim() == 'else if') {
+        let args = pack.words
+            .join('')
+            .split(/([(])/)
+            .slice(1)
+            .join('')
+            .split(/([)])/);
+        args = args[0] + args[1];
+        if (args == undefined) args = '';
+        result += 'else if' + args + '\n' +
+                '{\n' +
+                partcode(
+                    pack.words
+                        .join('')
+                        .split(/([)])/)
+                        .slice(2)
+                        .join(''),
+                ) + '}\n';
+      } else if (pack.words.join('').startsWith('else')) {
+        result += 'else' + '\n' +
+                    '{\n' +
+                    partcode(
+                        pack.words
+                            .join('')
+                            .trim()
+                            .substring(4),
+                    ) + '}\n';
+      } else if (pack.words.join('').split('(')[0].trim() == 'while') {
+        let args = pack.words
+            .join('')
+            .split(/([(])/)
+            .slice(1)
+            .join('')
+            .split(/([)])/);
+        args = args[0] + args[1];
+        if (args == undefined) args = '';
+        result += 'while' + args + '\n' +
+                '{\n' +
+                partcode(
+                    pack.words
+                        .join('')
+                        .split(/([)])/)
+                        .slice(2)
+                        .join('')) +
+                   '}\n';
+      } else if (pack.words.join('').split('(')[0].trim() == 'for') {
+        let args = pack.words
+            .join('')
+            .split(/([(])/)
+            .slice(1)
+            .join('')
+            .split(/([)])/);
+        if (!args[0].includes('in')) args[0]+=';i>=0;i--';
+        args = args[0] + args[1];
+        if (args == undefined) args = '';
+        result += 'for' + args + '\n' +
+                '{\n' +
+                partcode(
+                    pack.words
+                        .join('')
+                        .split(/([)])/)
+                        .slice(2)
+                        .join('')) +
+                   '}\n';
+      } else if (pack.words[0].trim() == '$') {
+        let args = pack.words[1];
+        if (args == undefined) args = '';
+        result += 'if(' + pack.words
+            .slice(1)
+            .join('')
+            .split(/([)])/)[0] +
+         pack.words
+             .join('')
+             .split(/([)])/)[1] +
+          ')\n' +
+                 '{\n' +
+                 partcode(
+                     pack.words
+                         .join('')
+                         .split(/([)])/)
+                         .slice(2)
+                         .join(''),
+                 ) + '}\n';
+      } else {
+        const ops = ['=', '!', '>', '<', '-', '+'];
+        let all = pack.words.join('');
+        all = all.replace('<<', '{');
+        all = all.replace('>>', '}');
+        const sp = pack.words.join('').split(/([(])/);
+        const body = sp.slice(1);
+        if (ops.some((el) => pack.words.join('').includes(el))) {
+          all = all.replace('.=', '='+pack.words[0].split('.')[0]+'.');
+          result += _strt + all + _end + ';\n';
+        } else if (pack.words.join('') != '') {
+          result += _strt + sp[0] + body.join('') + ';\n' + _end;
+        }
+      }
+    }
+  });
+  return result;
 }
